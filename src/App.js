2cc4fb895179cb9d90659448f8a41722e9d6cdea
rpasup <rajasekhar157@gmail.com>
2023-04-10 22:10:13 +0400

    updated with the stage 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.css'
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('../digital_team_dashboard/data.txt')
+      .then(response => response.text())
+      .then(text => {
+        const rows = text.split('\n');
+        const headers = rows[0].split(',');
+        const data = rows.slice(1).map(row => {
+          const values = row.split(',');
+          return headers.reduce((obj, header, index) => {
+            obj[header] = values[index];
+            return obj;
+          }, {});
+        });
+        setData(data);
+      })
+      .catch(error => console.error(error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>City</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row, index) => (
+            <tr key={index}>
+              <td>{row.Name}</td>
+              <td>{row.Email}</td>
+              <td>{row.City}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
