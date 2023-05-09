25a1d070738d129daf7eff379c818f4f53f1679e
rpasup <rajasekhar157@gmail.com>
2023-05-09 23:49:39 +0400

    nav_bar alignment and drop down toggele any where clickable

--- src/component/NavigationBar.js
+++ src/component/NavigationBar.js
@@ -6,59 +6,71 @@ import logo from '../RAKBANK_LOGO.png'
 import './NavigationBar.css';
 
 function NavigationBar() {
-    const getLastRefreshedTime = () => {
+  const getLastRefreshedTime = () => {
     const date = new Date();
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
     return `${hours}:${minutes}:${seconds}`;
-    };
+  };
+
   return (
-      <Navbar sticky="top" bg="dark" expand="xl">
-      {/* <Navbar.Brand href="/digital_team_dashboard"> */}
+    <Navbar sticky="top" bg="dark" expand="xl">
       <img
-          src={logo} // use your logo image here
-          height="35"
-          width="200"
-          className="d-inline-block align-top"
-          alt="Logo"
-        />
-      {/* </Navbar.Brand> */}
-      <Navbar.Text className="text-white font-weight-bold" style={{fontSize: "1.5rem"}}>
-      <a href="/digital_team_dashboard" className="text-white font-weight-bold" style={{textDecoration: "none"}}>
-        Digital Team Dashboard
-      </a>
+        src={logo}
+        height="35"
+        width="200"
+        className="d-inline-block align-top"
+        alt="Logo"
+      />
+      <Navbar.Text className="text-white font-weight-bold" style={{ fontSize: "1.5rem", marginLeft: "105px" }}>
+        <a href="/digital_team_dashboard" className="text-white font-weight-bold" style={{ textDecoration: "none" }}>
+          Digital Team Dashboard
+        </a>
       </Navbar.Text>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          {/* <Nav.Link><Link to="/digital_team_dashboard">Home</Link></Nav.Link> */}
-          <Nav.Link><Link to="/digital_team_dashboard/Deployment_Info" style={{ textDecoration: 'none' }}>Deployment Overview</Link></Nav.Link>
-          {/* <Dropdown style={{marginRight: "20px"}}>
-            <Dropdown.Toggle variant="dark" id="collasible-nav-dropdown">
-              Deployment Overview
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-              <Dropdown.Item><Link to="/digital_team_dashboard/Deployment_Info">Development</Link></Dropdown.Item>
-              
-              <Dropdown.Item><Link to="/digital_team_dashboard/Deployment_Info">UAT</Link></Dropdown.Item>
-              <Dropdown.Item><Link to="/digital_team_dashboard/Deployment_Info">Production</Link></Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-           */}
+          <Nav.Link><Link to="/digital_team_dashboard/Deployment_Info" style={{ textDecoration: 'none',marginLeft: "85px" }}>Deployment Overview</Link></Nav.Link>
           <Dropdown>
             <Dropdown.Toggle variant="dark" id="collasible-nav-dropdown">
               Projects Overview
             </Dropdown.Toggle>
             <Dropdown.Menu>
-              <Dropdown.Item><Link to="/digital_team_dashboard/Dev_projects_overview">Development</Link></Dropdown.Item>
-              <Dropdown.Item><Link to="/digital_team_dashboard/UAT_projects_overview">UAT</Link></Dropdown.Item>
-              <Dropdown.Item><Link to="/digital_team_dashboard/Production_projects_overview">Production</Link></Dropdown.Item>
+              <Dropdown.Item as={Link} to="/digital_team_dashboard/Dev_projects_overview">
+                Development
+              </Dropdown.Item>
+              <Dropdown.Item as={Link} to="/digital_team_dashboard/UAT_projects_overview">
+                UAT
+              </Dropdown.Item>
+              <Dropdown.Item as={Link} to="/digital_team_dashboard/Production_projects_overview">
+                Production
+              </Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
           <span className="last-refreshed-time">Last Refreshed Time: {getLastRefreshedTime()}</span>
         </Nav>
       </Navbar.Collapse>
+      <style>
+        {`
+          .navbar-nav {
+            display: flex;
+            align-items: center;
+          }
+
+          .nav-link {
+            margin-right: 20px;
+          }
+
+          .nav-link:last-child {
+            margin-right: 0;
+          }
+
+          .navbar-collapse {
+            justify-content: flex-end;
+          }
+        `}
+      </style>
     </Navbar>
   );
 }
