8d6ccd7308ee2bd1e86ad7a2a1fdaa18e0ec12b0
rpasup <rajasekhar157@gmail.com>
2023-05-20 20:24:35 +0400

    added the deployment details with sort opition

--- src/component/Deploymentdetails.js
+++ src/component/Deploymentdetails.js
@@ -1,98 +1,160 @@
 import React, { useState, useEffect } from 'react';
-import { FaCheckCircle } from "react-icons/fa";
-import { FaTimesCircle } from "react-icons/fa";
+import { FaCheckCircle } from 'react-icons/fa';
+import { FaTimesCircle } from 'react-icons/fa';
 import { BsFillFileEarmarkTextFill } from 'react-icons/bs';
-import './Deploymentdetails.css'
+import { useTable, useSortBy } from 'react-table';
+import './Deploymentdetails.css';
 
 function Deploymentdetails() {
   const [data, setData] = useState([]);
-  const [filterText, setFilterText] = useState("");
+  const [filterText, setFilterText] = useState('');
   const [maintenanceMessage, setMaintenanceMessage] = useState(null);
 
   const getStatusIcon = (status) => {
-    if (status === "up_running") {
-      return <FaCheckCircle color="green"/>;
+    if (status === 'up_running') {
+      return <FaCheckCircle color="green" />;
     } else {
-      return <FaTimesCircle color="red"/>;
+      return <FaTimesCircle color="red" />;
     }
   };
 
   useEffect(() => {
     fetch('../digital_team_dashboard/deployment_details.txt')
-      .then(response => response.text())
-      .then(text => {
+      .then((response) => response.text())
+      .then((text) => {
         const rows = text.trim().split('\n');
-        // const rows = text.split('\n');
         const headers = rows[0].split(',');
-        const data = rows.slice(1).map(row => {
-          const values = row.split(',');
-          return headers.reduce((obj, header, index) => {
-            obj[header] = values[index];
-            return obj;
-          }, {});
-        });
+        const data = rows
+          .slice(1)
+          .map((row) => row.split(','))
+          .map((values) =>
+            headers.reduce((obj, header, index) => {
+              obj[header] = values[index];
+              return obj;
+            }, {})
+          );
         setData(data);
       })
-      .catch(error => console.error(error));
+      .catch((error) => console.error(error));
 
-      fetch('../digital_team_dashboard/maintenance_updates.txt')
-      .then(response => response.text())
-      .then(text => {
+    fetch('../digital_team_dashboard/maintenance_updates.txt')
+      .then((response) => response.text())
+      .then((text) => {
         setMaintenanceMessage(text.trim());
       })
-      .catch(error => console.error(error));
+      .catch((error) => console.error(error));
   }, []);
 
-  const filteredData = data.filter((row) =>
-    row["Application"] && row["Application"].toLowerCase().includes(filterText.toLowerCase())
-);
+  const columns = React.useMemo(
+    () => [
+      { Header: 'Environment', accessor: 'Environment' },
+      { Header: 'Application', accessor: 'Application' },
+      { Header: 'DeploymentStatus', accessor: 'DeploymentStatus' },
+      {
+        Header: 'Application_Status',
+        accessor: 'Application_Status',
+        Cell: ({ value }) => (
+          <div title={value}>{getStatusIcon(value)}</div>
+        ),
+      },
+      { Header: 'Build_Number', accessor: 'Build_Number' },
+      {
+        Header: 'Logs',
+        accessor: 'logs',
+        Cell: ({ value }) => (
+          <a href={value} target="_blank" rel="noopener noreferrer">
+            <BsFillFileEarmarkTextFill />
+          </a>
+        ),
+      },
+      { Header: 'Deployed_By', accessor: 'DeployedBy' },
+      {
+        Header: 'Last_Deployed_On',
+        accessor: 'Last_Deployed_On',
+        Cell: ({ value }) => <div className="word-wrap">{value}</div>,
+      },
+    ],
+    []
+  );
 
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: [{ id: 'Application', desc: false }], // Set initial sorting by Application column in ascending order
+    },
+    useSortBy
+  );
+
+  const handleFilterChange = (e) => {
+    setFilterText(e.target.value);
+  };
+
+  const filteredData = rows.filter((row) =>
+    row.original['Application']
+      .toLowerCase()
+      .includes(filterText.toLowerCase())
+  );
 
   return (
     <div className="deployment-details">
       <div className="filter">
-      {maintenanceMessage && (
-        <div className="maintenance-message"><span>{maintenanceMessage}</span></div>
-      )}
+        {maintenanceMessage && (
+          <div className="maintenance-message">
+            <span>{maintenanceMessage}</span>
+          </div>
+        )}
         {/* //Filter By Application: */}
         <span></span>
         <input
           type="text"
           placeholder="Search by Application..."
           value={filterText}
-          onChange={(e) => setFilterText(e.target.value)}
+          onChange={handleFilterChange}
         />
       </div>
-      <table>
+      <table {...getTableProps()}>
         <thead>
-          <tr>
-            <th>Environment</th>
-            <th>Application</th>
-            <th>DeploymentStatus</th>
-            <th>Application_Status</th>
-            <th>Build_Number</th>
-            <th>Logs</th>
-            <th>Deployed_By</th>
-            <th>Last_Deployed_On</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredData.map((row, index) => (
-            <tr key={index}>
-              <td>{row.Environment}</td>
-              <td>{row.Application}</td>
-              <td>{row.DeploymentStatus}</td>
-              <td title={row.Application_Status}> {getStatusIcon(row.Application_Status)}</td>
-              <td>{row.Build_Number}</td>
-              <td>
-                <a href={row.logs} target="_blank" rel="noopener noreferrer">
-                  <BsFillFileEarmarkTextFill />
-                </a>
-              </td>
-              <td>{row.DeployedBy}</td>
-              <td className="wrap-text">{row.Last_Deployed_On}</td>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render('Header')}
+                  <span>
+                    {column.isSorted ? (
+                      column.isSortedDesc ? (
+                        <i className="fa fa-caret-up"></i>
+                      ) : (
+                        <i className="fa fa-caret-down"></i>
+                      )
+                    ) : (
+                      ''
+                    )}
+                  </span>
+                </th>
+              ))}
             </tr>
           ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {filteredData.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
